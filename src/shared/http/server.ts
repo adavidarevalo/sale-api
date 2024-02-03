98546cccab28171ae31fc34ef08697af56236976
adavidarevalo <davidarevaloc20@gmail.com>
2024-02-03 12:33:51 -0700

    Cahnged server port

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -46,6 +46,6 @@ app.use(
     },
 );
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000! 🏆');
+app.listen(3001, () => {
+    console.log('Server started on port 3001! 🏆');
 });
